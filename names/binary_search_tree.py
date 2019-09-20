12c8e10fa5eb94af26a788db682628fd77d4781a
Sam <Jeffcoat>
2019-09-20 11:23:44 -0500

    Adds BST for search

---
+++ names/binary_search_tree.py
@@ -0,0 +1,17 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left= BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+                if not self.right:
+                    self.right = BinarySearchTree(value)
+                else:
+                    self.right.insert(value)
