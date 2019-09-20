eb8c8246ab168929e45ea20faa456d05338822fd
Sam <Jeffcoat>
2019-09-20 11:43:40 -0500

    Adds pseudocode for bst

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,12 +11,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
+"""duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
 
+# use our binary search tree to optimize
+# need to import our bst
+#iterate through our names list using our insert method that we alread have pre built
+# insort those names into names
+# go through name_2
+# check to see if any names in 2 are new
+# if so, append those duplicates
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
